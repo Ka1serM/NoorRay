8047fadcf8fae87aeb330a0a186f7fe3a187b4ab
Marcel K. <106357974+Ka1serM@users.noreply.github.com>
2025-08-08 17:24:00 +0200

    mac build action

---
+++ src/Shaders/recompile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GLSLC="$VULKAN_SDK/Bin/glslc"
+
+echo "Compiling shaders..."
+
+$GLSLC PathTracing/RTX/RayGeneration.glsl -o PathTracing/RayGeneration.spv --target-env=vulkan1.3
+$GLSLC PathTracing/RTX/ClosestHit.glsl -o PathTracing/ClosestHit.spv --target-env=vulkan1.3
+$GLSLC PathTracing/RTX/Miss.glsl -o PathTracing/Miss.spv --target-env=vulkan1.3
+
+$GLSLC PathTracing/PathTracer.comp -o PathTracing/PathTracer.spv --target-env=vulkan1.3 -DUSE_COMPUTE=1
+
+$GLSLC Tonemapper.comp -o Tonemapper.spv --target-env=vulkan1.3
+
+echo "Shader compilation complete."

8dbb7c51099dd4b27b294c0ec94e0806299fbb22
Marcel K. <106357974+Ka1serM@users.noreply.github.com>
2026-07-08 14:14:10 +0200

    default sensor resolution 1280x720

--- src/Camera/Sensor.h
+++ src/Camera/Sensor.h
@@ -10,8 +10,8 @@
 struct Sensor {
     float widthMm{5.784f};
     float heightMm{3.264f};
-    uint32_t resolutionWidth{1928};
-    uint32_t resolutionHeight{1088};
+    uint32_t resolutionWidth{1280};
+    uint32_t resolutionHeight{720};
 
     NR_CPU_GPU glm::uvec2 resolution() const { return {resolutionWidth, resolutionHeight}; }
     NR_CPU_GPU void setResolution(uint32_t w, uint32_t h) { resolutionWidth = std::max(1u, w); resolutionHeight = std::max(1u, h); }
